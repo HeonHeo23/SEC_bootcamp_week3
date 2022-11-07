f4adb6950fabd37cd0ae78b4642fe265ed829d94
HeonHeo23 <chrisheo23@gmail.com>
2022-11-08 01:45:21 +0900

    finished ch13 and ch14

---
+++ Ch13/model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 2001
+    },
+    Editor: Number,
+    Admin: Number,
+  }
+});
+
+module.exports = mongoose.model('Employee', userSchema);
+//Mongoose changes the model name by adding s at the end & lowercase the first
